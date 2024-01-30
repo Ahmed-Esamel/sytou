456270f3911fd966a3082b760a68b10de0c4a13f
Ahmed-Esame <sytoua3@gmail.com>
2024-01-30 19:12:09 +0200

    UpSkill

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -4,14 +4,27 @@ namespace sytou\test;
 
 use Illuminate\Support\ServiceProvider;
 
-class  TestServiceProvider extends ServiceProvider{
-    public function boot(){
+class TestServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // تحميل ملفات التوجيه (Routes)
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        // تحميل المهام (Migrations)
         $this->loadMigrationsFrom(__DIR__.'/Migrations');
-        $this->loadViewsFrom(__DIR__.'/views', 'wdd.slug');
+
+        // تحميل المشاهدات (Views)
+        $this->loadViewsFrom(__DIR__.'/views', 'wdd_slug');
+
+        // نشر المهام (Publish Migrations)
+        $this->publishes([
+            __DIR__.'/Migrations' => database_path('migrations')
+        ], 'slug-migrations');
     }
 
-    public function require(){
-        
+    public function register()
+    {
+        // التسجيل
     }
 }
